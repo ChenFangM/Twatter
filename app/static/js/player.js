92dd57fa16862c67d3e34b3b4d43f9bb45c4b90e
zeeyingg <zjian30@stuy.edu>
2022-12-20 20:22:18 -0500

    pause/play buttons created but dunut work :(

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -1,41 +1,17 @@
 function switchStations() {
-  // console.log("got clicked.");
+  console.log("switchStations() clicked.");
   var frame = document.getElementById("player");
   frame.src = "https://www.youtube.com/embed/L1zoItQw_e4?controls=0&autoplay=1&mute=0";
 }
 
-// global variable for the player
-var player;
-
-// this function gets called when API is ready to use
-function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
-  player = new YT.Player("player", {
-    events: {
-      // call this function when player is ready to use
-      'onReady': onPlayerReady
-    }
-  });
+function pause(){
+  console.log("pause() clicked.");
+  var frame = document.getElementById("player")
+  frame.contentWindow.postMessage('{"event":"command","func":"PauseVideo","args":""}', '*');
 }
 
-function onPlayerReady(event) {
-  // // bind events
-  // var playButton = document.getElementById("play-button");
-  // playButton.addEventListener("click", function () {
-  //   player.playVideo();
-  // });
-
-  var pauseButton = document.getElementById("pause-button");
-  pauseButton.addEventListener("click", function ()
-   {
-    console.log('pause clicked');
-    player.pauseVideo();
-  }
-);
+function play(){
+  console.log("play() clicked.");
+  var frame = document.getElementById("player")
+  frame.contentWindow.postMessage('{"event":"command","func":"PlayVideo","args":""}', '*');
 }
-
-//injecting YouTube API script
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
