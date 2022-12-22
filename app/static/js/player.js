22b4527835deb1f91bf4a381143d9b3f3a2efdf9
Ziying Jian <zjian30@stuy.edu>
2022-12-21 22:09:28 -0500

    RADIO CAPABILITIES DONE -- UNSTOPPABLE NOW

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -1,8 +1,44 @@
+const videoLinks =
+[
+
+    "https://www.youtube.com/embed/L1zoItQw_e4?controls=0&autoplay=1&mute=0",
+
+    "https://www.youtube.com/embed/uYJmRShQYH0?autoplay=1&mute=0&controls=0&playsinline=1&showinfo=0&rel=0&modestbranding=1&color=black&enable&amp;wmode=transparent",
+
+    "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1&mute=0&controls=0&playsinline=1&showinfo=0&rel=0&modestbranding=1&color=black&enable&amp;wmode=transparent",
+
+    "https://www.youtube.com/embed/qH3fETPsqXU?autoplay=1&mute=0&controls=0&playsinline=1&showinfo=0&rel=0&modestbranding=1&color=black&enable&amp;wmode=transparent?enablejsapi=1"
+]
+
+const videoTitles =
+[
+
+  "Holiday Mood 🎅 Christmas Lofi 🎄Christmas 2023 [chill lo-fi hip hop beats]",
+
+  "Chill Vibes 🍀 Stop Overthinking - Lofi hip hop mix - Calm Down And Relax",
+
+  "lofi hip hop radio - beats to relax/study to",
+
+  "【24/7 CHILL LOFI HIP HOP RADIO】beats to sleep/relax/study to"
+]
+
+
 function switchStations() {
   console.log("switchStations() clicked.");
   var frame = document.getElementById("player");
-  frame.src = "https://www.youtube.com/embed/L1zoItQw_e4?controls=0&autoplay=1&mute=0";
+  // frame.src = "https://www.youtube.com/embed/L1zoItQw_e4?controls=0&autoplay=1&mute=0";
+  // var jsonData = JSON.parse("../json/links.json"); // Parsing wouldn't work :(
+  // var jsonData = JSON.parse(data);
+  console.log(videoLinks);
+  frame.src = videoLinks.shift();
+  videoLinks.push(frame.src);
+
+  var title = document.getElementById("video-title");
+  console.log(videoTitles);
+  title.innerHTML = "Now Playing " + videoTitles.shift()
+  videoTitles.push(title);
 }
+
 
 function pause(){
   console.log("pause() clicked.");
